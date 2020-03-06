ca3ac321735732ea4d0cb84f0752f909109e3b1e
jplineb <38058488+jplineb@users.noreply.github.com>
2020-03-06 13:24:12 -0500

    Created Script to visualise statistics

---
+++ Code/mortality_checker.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  6 10:47:45 2020
+
+@author: John Paul
+"""
+
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('time_series_19-covid-Deaths.csv')
---
+++ csse_covid_19_data/csse_covid_19_time_series/mortality_checker.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  6 10:47:45 2020
+
+@author: John Paul
+"""
+
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+df_death = pd.read_csv('time_series_19-covid-Deaths.csv')
+df_recovered = pd.read_csv('time_series_19-covid-Recovered.csv')
+df_confirmed = pd.read_csv('time_series_19-covid-Confirmed.csv')
+
+# test of code
+total_deaths = df_death['3/5/20'].sum()
+total_recovered = df_recovered['3/5/20'].sum()
+
+mortality_rate = total_deaths/total_recovered
+
+print(mortality_rate)
+#######################################
+
+time_deaths = df_death.sum().tolist()[2:]
+time_recovers = df_recovered.sum().tolist()[2:]
+time_confirms = df_confirmed.sum().tolist()[2:]
+
+dates = df_death.columns.tolist()[4:]
+
+
+mortality = []
+for x,y in zip(time_deaths,time_confirms):
+    mortality.append(x/y)
+
+
+df_mortality = pd.DataFrame(zip(dates,mortality))
+plt.figure(figsize=(10,7))
+plt.plot(dates, mortality)
+plt.title('Mortality Rate')
+plt.xticks(rotation=90)
+plt.ylabel('%')
+plt.xlabel('Dates')
+plt.autoscale()
+
+plt.figure(2, figsize=(10,7))
+plt.plot(dates, time_deaths, label='Deaths')
+plt.plot(dates, time_recovers, label='Recovered')
+plt.plot(dates, time_confirms, label= 'Confirmed')
+plt.legend()
+plt.xticks(rotation=90)
+plt.ylabel('stats')
+plt.xlabel('Dates')
+plt.autoscale()
+
